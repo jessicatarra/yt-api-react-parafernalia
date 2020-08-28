a11b9f1707aaf5451888aac18f10c7318f7374bd
Jessica Tarra <jessictarra@gmail.com>
2020-08-28 14:16:33 -0300

    Updates

---
+++ precache-manifest.8179c1d7a79d61866e6c24e59d0e000c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c4c4758f18236728c61fcf59144894",
+    "url": "/yt-api-react-parafernalia/index.html"
+  },
+  {
+    "revision": "33b94417257925df7aac",
+    "url": "/yt-api-react-parafernalia/static/css/main.d5d5ece7.chunk.css"
+  },
+  {
+    "revision": "4038cb41ad3b840e4e41",
+    "url": "/yt-api-react-parafernalia/static/js/2.f5712f83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yt-api-react-parafernalia/static/js/2.f5712f83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b94417257925df7aac",
+    "url": "/yt-api-react-parafernalia/static/js/main.50aa7ab1.chunk.js"
+  },
+  {
+    "revision": "bd2a31d3dbf893a20447",
+    "url": "/yt-api-react-parafernalia/static/js/runtime-main.068f9bed.js"
+  }
+]);
